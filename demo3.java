58a5e76579fffa750bb31631146f933a64cbcb9c
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-02 12:11:38 +0530

    Add files via upload
    This is the greatest of 3 numbers

---
+++ demo3.java
@@ -0,0 +1,21 @@
+public class demo3 {
+    public static void main(String[] args) {
+        // Define three numbers
+        int num1 = 45;
+        int num2 = 78;
+        int num3 = 32;
+
+        // Determine the greatest number
+        int greatest;
+        if (num1 >= num2 && num1 >= num3) {
+            greatest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            greatest = num2;
+        } else {
+            greatest = num3;
+        }
+
+        // Display the greatest number
+        System.out.println("The greatest number is: " + greatest);
+    }
+}
