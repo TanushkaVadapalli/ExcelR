18b9061ac4aad967777120903e470dde5c530fd4
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-03 10:11:26 +0530

    Name and pwd in while loop condition

---
+++ demo6.java
@@ -0,0 +1,11 @@
+public class demo6 {
+    public static void main(String[] args) {
+        String username = "Tanushka";
+        String pwd = "tanushka@123";
+        while ((username == "Tanushka") && (pwd == "tanushka@123")) {
+           System.out.println("hello Tanushka Welcome");
+            break;
+       
+        }
+    }
+}
