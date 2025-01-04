64f5ab58e10fad2d08a6fcb4797f48278561d934
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-04 10:42:34 +0530

    user input credentials

---
+++ demo7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class demo7{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.print("Enter your phoneNo: ");
+        long phoneNo = scanner.nextLong();
+        scanner.close();
+    }
+}
