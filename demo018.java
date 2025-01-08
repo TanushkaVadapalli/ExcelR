e10eff3513853a1715da764623221e9fbac1b7cf
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-08 10:06:25 +0530

    Round Off values

---
+++ demo018.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class demo018 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        int roundedNumber = ((number + 50) / 100) * 100;
+        System.out.println("The number rounded off to the nearest multiple of 100 is: " + roundedNumber);
+        scanner.close();
+
+
+
+
+    }
+}
