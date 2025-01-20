e399b1560d6eff72bf844161322c3e50a0870169
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-20 10:03:57 +0530

    Array List

---
+++ demo029.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class demo029 {
+    public static void main (String[] args){
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("Tanu");
+        friends.add("Thanoj");
+        friends.add("Tharun");
+        friends.add("Rishi");
+        friends.add("Sai");
+        friends.add("Ganesh");
+        friends.add("Goverdhan");
+        friends.add("Dheeraj");
+        friends.add("Tharuni");
+        friends.add("Tarak");
+        System.out.println(friends);
+
+    } 
+
+
+}
