0215adb68068152a3348da693eccd392ca50edd7
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-24 09:52:23 +0530

    Update demo2.java

--- demo2.java
+++ demo2.java
@@ -4,14 +4,13 @@ public class demo2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Input two numbers from the user
+     
         System.out.print("Enter the first number: ");
         int num1 = scanner.nextInt();
         
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
         
-        // Determine the greatest number
         if (num1 > num2) {
             System.out.println(num1 + " is greater than " + num2);
         } else if (num2 > num1) {
