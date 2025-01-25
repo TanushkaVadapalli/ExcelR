5bdb67170a0bb8a0989e26033b4c8b0b192b7547
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-25 10:24:45 +0530

    Even or Odd

---
+++ demo034.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class demo034 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num1 = scanner.nextInt();
+        if (num1 %2 == 0) {
+            System.out.println("num + is even");
+            
+        }else {
+            System.out.println("num + is odd");
+        }
+   
+       }
+
+           
+       }
+            
+        
+    
+
