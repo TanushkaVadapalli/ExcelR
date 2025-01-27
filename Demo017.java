203d9594a7e753a7516a7d85884b79db5135fd49
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-27 09:37:19 +0530

    Update and rename demo017.java to Demo017.java

--- Demo017.java
+++ Demo017.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo017 {
+public class Demo017 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter two-digit number: ");
