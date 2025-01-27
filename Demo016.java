f5b5490bc82c12a7a8743f295e9cf3c2930ded5d
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-01-27 09:36:57 +0530

    Update and rename demo016.java to Demo016.java

--- Demo016.java
+++ Demo016.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo016{
+public class Demo016{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter a name: ");
